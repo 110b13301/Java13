819ce01ca2c3a4bf8df7ca80d2c3b889cb3bf5ce
110b13301 <112999887+110b13301@users.noreply.github.com>
2022-12-13 09:11:56 +0800

    Add files via upload

---
+++ Java03.java
@@ -0,0 +1,14 @@
+public class Java03 {
+	public static void main(String[] args) {
+		int num=12, den=0;
+		try {
+			int ans=num/den;
+		}
+		catch(ArithmeticException e) { // get exception "ArithmeticException"
+			System.out.println("除數為0");
+		}
+		finally {
+			System.out.println("end of the main()!!");
+		}
+	}
+}
